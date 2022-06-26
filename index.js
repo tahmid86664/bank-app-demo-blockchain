2f54d7cef3089918009cead9d3b6d9e0b21dbaae
Tahmid Khandokar <tahmidkhandokar82@gmail.com>
2022-06-26 10:45:42 +0600

    some packages added and port changed for local

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const cors = require("cors");
 
 const connectDB = require("./db/connect");
 
 const userRoutes = require("./routes/userRoutes");
 const transactionRoutes = require("./routes/transactionRoutes");
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 9000;
 
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+  })
+);
 app.use(express.json());
 app.use("/users", userRoutes);
 app.use("/transactions", transactionRoutes);
